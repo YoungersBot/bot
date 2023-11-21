fef37976d26de2d38130ed02da18f383bfac1cc4
dilbazi <you@example.com>
2023-11-21 23:28:46 +0400

    weather

--- app/bot_utils/buttons.py
+++ app/bot_utils/buttons.py
@@ -8,6 +8,8 @@ class ButtonsText:
     subscribes: str = "Ваши подписки"
     five_cheapest: str = "5 самых дешёвых билетов"
     weather: str = "Погода"
+    weather_in_your_city: str = "Погода в твоем городе"
+    weather_in_any_city: str = "Погода в любом городе"
 
 
 buttons = ButtonsText()
---
+++ app/weather_service.py
@@ -0,0 +1,73 @@
+import asyncio
+
+from aiogram import F, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
+
+from aviasales_api import AviasalesAPI
+from bot_utils.answers import answers
+from bot_utils.buttons import buttons
+from bot_utils.keyboards import KeyboardBuilder
+from find_airport import airports_finder
+from weather_api import WeatherApi
+
+router = Router()
+
+my_coor = []
+
+
+class WeatherState(StatesGroup):
+    any_city_state = State()
+
+
+@router.message(F.content_type == "location")
+async def location(message: Message) -> None:
+    my_coor.append(message.location.latitude)
+    my_coor.append(message.location.longitude)
+    user_coords = (message.location.latitude, message.location.longitude)
+    print(user_coords)
+    nearest_airport = airports_finder.find_nearest_airport(user_coords)
+
+    response_city_info = asyncio.create_task(AviasalesAPI.get_city_with_airport_code(nearest_airport))
+    in_city, country, airport = await response_city_info
+    await message.answer(answers.geolocation.format(in_city=in_city, country=country, airport=airport))
+
+
+@router.message(lambda message: message.text == buttons.weather)
+async def weather_in_your_city_handler(message: Message) -> None:
+    kb = [
+        [KeyboardButton(text=buttons.weather_in_your_city), KeyboardButton(text=buttons.weather_in_any_city)],
+    ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    await message.answer(answers.actions, reply_markup=keyboard)
+
+
+@router.message(lambda message: message.text == buttons.weather_in_your_city)
+async def your_city(message: Message):
+    weather_your_city = WeatherApi.get_weather_with_coor(my_coor[0], my_coor[1])
+    result = await weather_your_city
+    await message.reply(answers.weather_in_your_city.format(result=result))
+
+
+@router.message(lambda message: message.text == buttons.weather_in_any_city)
+async def any_city(message: Message, state: FSMContext) -> None:
+    await state.set_state(WeatherState.any_city_state)
+    await message.answer(answers.weather)
+
+
+@router.message(WeatherState.any_city_state)
+async def weather_any_city(message: Message, state: FSMContext) -> None:
+    weather_response = WeatherApi.get_weather(message.text)
+    result = await weather_response
+    await message.reply(answers.weather_in_any_city.format(result=result))
+    await state.clear()
+
+
+@router.message()
+async def echo_handler(message: Message) -> None:
+    main_keyboard = KeyboardBuilder.main_reply_keyboard()
+    try:
+        await message.send_copy(chat_id=message.chat.id, reply_markup=main_keyboard)
+    except TypeError:
+        await message.answer("Nice try!")
